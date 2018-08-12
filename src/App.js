d959291a24122dbdc3d6e189baf207fde4b0025f
DemanPierre <leomarcouf@hotmail.com>
2018-08-12 19:46:32 +0200

    Mettre API credentials dans un fichiers séparé.

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import scriptLoader from 'react-async-script-loader';
+import { MAP_KEY } from '../data/credentials'
+import { mapStyles } from '../data/mapStyles.js';
+import ListView from './ListView'
+import spinner from '../images/circles-loader.svg';
+import foursquare from '../images/foursquare.png';
+
+class App extends Component {
+
+  state = {
+    listOpen: true,
+    map: {},
+    infowindow: {},
+    bounds: {},
+    mapReady: false,
+    // future use - location search
+    mapCenter : { lat: 40.346074, lng: -74.067858 }
+  }
+
+
+
+  componentWillReceiveProps({isScriptLoadSucceed}){
+
+    // Check if script is loaded and if map is defined
+    if (isScriptLoadSucceed && !this.state.mapReady ) {
+
+      // create map
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: this.state.mapCenter,
+        styles: mapStyles
+      });
+
+      // set up bounds and infowindow to use later
+      const bounds = new window.google.maps.LatLngBounds();
+      const infowindow = new window.google.maps.InfoWindow({maxWidth: 300});
+
+      this.setState({
+        map: map,
+        infowindow: infowindow,
+        bounds: bounds,
+        mapReady: true,
+      });
+
+    // alert user if map request fails
+    } else if ( !this.state.mapReady ) {
+      alert("Map did not load");
+    }
+  }
+
+  toggleList = () => {
+    this.setState( { listOpen: !this.state.listOpen})
+  }
+
+  render() {
+
+    const { listOpen, map, infowindow, bounds, mapReady, mapCenter } = this.state;
+    const { toggleList } = this.props;
+
+    return (
+      <div className="container">
+        <div id="menu" className="toggle" onClick={this.toggleList}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
+          </svg>
+        </div>
+        <div className={ listOpen ? "list open" : "list"}>
+          <h2 className="appTitle">Red Bank Eats</h2>
+          <hr />
+          { /* render markers only when map has loaded */
+            mapReady ?
+            <ListView
+              map={map}
+              infowindow={infowindow}
+              bounds={bounds}
+              mapCenter={mapCenter}
+              toggleList={this.toggleList}
+              listOpen={listOpen}
+            />
+            : <p className="error"> Map has not loaded </p>
+          }
+          <img src={foursquare} alt="Powered by Foursquare" className="fs-logo"/>
+        </div>
+        <div id="map" className="map">
+            <div className="loading">
+              <h4 className="loading-message">Map is loading...</h4>
+              <img src={spinner} className="spinner" alt="loading indicator" />
+           </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default scriptLoader(
+    [`https://maps.googleapis.com/maps/api/js?key=${MAP_KEY}`]
+)(App);
---
+++ src/ListView.js
@@ -0,0 +1,213 @@
+import React, { Component } from 'react';
+import { CLIENT_ID, CLIENT_SECRET } from '../data/credentials'
+import noImage from '../images/no-image-available.png';
+import fsButton from '../images/foursquare-button.png';
+import foodIcon from '../images/food-marker.png';
+import PropTypes from 'prop-types';
+
+class ListView extends Component {
+
+  static propTypes = {
+    map: PropTypes.object.isRequired,
+    infowindow: PropTypes.object.isRequired,
+    bounds: PropTypes.object.isRequired,
+    mapCenter: PropTypes.object.isRequired,
+    toggleList: PropTypes.func.isRequired,
+    listOpen: PropTypes.bool.isRequired
+  }
+
+  state = {
+    query: '',
+    allPlaces: [],
+    filteredPlaces: []
+  }
+
+  componentDidMount () {
+    this.getFSLocations()
+    .then( places => this.addMarkers(places));
+  }
+
+  addMarkers (places) {
+    const { map, bounds } = this.props;
+    const self = this;
+
+    places.forEach( (location) =>  {
+
+      const position = {
+        lat: location.location.lat,
+        lng: location.location.lng
+      }
+
+      location.marker = new window.google.maps.Marker({
+        position: position,
+        map: map,
+        title: location.name,
+        id: location.id,
+        icon: foodIcon
+      });
+
+      bounds.extend(position);
+
+      location.marker.addListener('click', function() {
+        const marker = this;
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+          marker.setAnimation(null);
+        }, 2100);
+        self.getFSDetails(marker)
+      });
+
+    });
+
+    // size and center map
+    map.fitBounds(bounds);
+  }
+  getFSLocations = () => {
+    const { mapCenter } = this.props;
+    const fSURL = 'https://api.foursquare.com/v2/venues/';
+    const VERS = '20171227';
+    const CATEGORIES = {
+      american: '4bf58dd8d48988d14e941735',
+      asian: '4bf58dd8d48988d142941735',
+      pub: '4bf58dd8d48988d11b941735',
+      italian:  '4bf58dd8d48988d110941735',
+      indian: '4bf58dd8d48988d10f941735',
+      greek: '4bf58dd8d48988d10e941735',
+      french: '4bf58dd8d48988d10c941735',
+      diner: '4bf58dd8d48988d147941735',
+      mediterranean: '4bf58dd8d48988d1c0941735',
+      mexican: '4bf58dd8d48988d1c1941735',
+      middleEastern: '4bf58dd8d48988d115941735',
+      steakhouse: '4bf58dd8d48988d1cc941735',
+      vegetarian: '4bf58dd8d48988d1d3941735'
+    }
+
+    const CATEGORY_ID = Object.keys(CATEGORIES).map((cat) => CATEGORIES[cat]);
+    const RADIUS = '1250'
+//https://stackoverflow.com/questions/39245994/use-fetch-to-send-get-request-with-data-object
+    const requestURL = `${fSURL}search?ll=${mapCenter.lat},${mapCenter.lng}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERS}&categoryId=${CATEGORY_ID}&radius=${RADIUS}&limit=50`
+    return fetch(requestURL)
+    .then(response => response.json())
+    .then(data => {
+      const places = data.response.venues;
+      const goodPlaces = places.filter( place => place.location.address && place.location.city && place.location.city == "Red Bank");
+
+      // sort before updating state
+      goodPlaces.sort(this.sortName);
+      this.setState({
+        allPlaces: goodPlaces,
+        filteredPlaces: goodPlaces
+      });
+      return goodPlaces;
+    })
+     .catch(error => alert('Foursquare request failed'));
+  }
+
+  getFSDetails = (marker) => {
+    const { map, infowindow } = this.props;
+    const fSURL = 'https://api.foursquare.com/v2/venues/';
+    const FSID =  marker.id; //'412d2800f964a520df0c1fe3';
+    const VERS = '20171227';
+
+    const requestURL = `${fSURL}${FSID}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERS}`
+    fetch(requestURL)
+    .then(response => response.json())
+    .then(data => {
+      const place = data.response.venue;
+      console.log(place)
+      marker.url = place.canonicalUrl ? place.canonicalUrl : 'https://foursquare.com/';
+      marker.photo = place.bestPhoto ? place.bestPhoto.prefix +
+                'width100' + place.bestPhoto.suffix
+                : noImage;
+      marker.phone = place.contact && place.contact.formattedPhone ? place.contact.formattedPhone : '';
+      marker.address = place.location.address;
+      marker.category = place.categories.length > 0 ? place.categories[0].name : '';
+      marker.price = place.attributes.groups[0].summary &&  place.attributes.groups[0].type === "price" ?
+                     place.attributes.groups[0].summary : '';
+      marker.tip = place.tips.count > 0 ? `"${place.tips.groups[0].items[0].text}"` : 'No tips available';
+      marker.infoContent = `<div class="place">
+                              <img class="place-photo" src=${marker.photo} alt="${marker.title}">
+                              <div class="place-meta">
+                                <h2 class="place-title">${marker.title}</h2>
+                                <p class="place-data">${marker.category}</p>
+                                <p class="place-price">${marker.price}</p>
+                                <p class="place-contact">${marker.address}</p>
+                                <a class="place-phone" href="tel:${marker.phone}">${marker.phone}</a>
+                              </div>
+                            </div>
+                            <p class="place-tip">${marker.tip}</p>
+                            <a class="place-link" href="${marker.url}" target="_blank">
+                              <span>Read more</span>
+                              <img class="fs-link" src="${fsButton}">
+                            </a>`
+      infowindow.setContent(marker.infoContent);
+      infowindow.open(map, marker);
+      if (this.props.listOpen) this.props.toggleList();
+    })
+     .catch(error => alert('Foursquare request failed'));
+  }
+  filterPlaces = (event) => {
+    const { allPlaces } = this.state;
+    const query = event.target.value.toLowerCase();
+    this.setState({ query: query })
+    const filteredPlaces = allPlaces.filter((place) => {
+      const match = place.name.toLowerCase().indexOf(query) > -1;
+      place.marker.setVisible(match);
+      return match;
+    })
+
+    // sort before updating state
+    filteredPlaces.sort(this.sortName);
+
+    this.setState({filteredPlaces: filteredPlaces })
+  }
+
+  sortName = (a, b) => {
+    // remove case senstivity
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // if names are equal
+    return 0;
+  };
+
+  showInfo = (place) => {
+    // force marker click
+    window.google.maps.event.trigger(place.marker,'click');
+  }
+
+  render() {
+    const {filteredPlaces, query} = this.state;
+
+    return (
+      <div className="list-view">
+        <input type="text"
+          placeholder="filter by name"
+          value={ query }
+          onChange={ this.filterPlaces }
+          className="query"
+        />
+        {filteredPlaces.length > 0 ?
+        <ul className="places-list">
+          {filteredPlaces.map((place, id) =>
+          <li
+            key={id}
+            className="place"
+            onClick={this.showInfo.bind(this, place)}
+            >{place.name}
+          </li>
+          )}
+        </ul>
+        : <p className="error">No places match filter</p>
+        }
+      </div>
+    );
+  }
+}
+
+export default ListView;
